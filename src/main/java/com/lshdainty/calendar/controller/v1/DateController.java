e1a10b9cc3ccb5ccf10b442de1a4258a540f806e
lshdainty <lshdainty@gmail.com>
2023-09-04 16:59:31 +0900

    add date Login

---
+++ src/main/java/com/lshdainty/calendar/controller/v1/DateController.java
@@ -0,0 +1,28 @@
+package com.lshdainty.calendar.controller.v1;
+
+import java.util.*;
+
+import org.springframework.web.bind.annotation.*;
+// import org.springframework.stereotype.*;
+// import org.springframework.beans.factory.annotation.*;
+
+@RestController("dateController")
+@RequestMapping(value={"${apiPrefixV1}/dates"})
+public class DateController {
+    @GetMapping("/")
+    public String date() {
+        return "date commit";
+    }
+
+    @GetMapping("/date")
+    public Map<String, Object> dateController() {
+        Calendar cal = Calendar.getInstance();
+        System.out.println(cal.get(Calendar.YEAR));
+        System.out.println(cal.get(Calendar.MONTH));
+        System.out.println(cal.get(Calendar.DATE));
+
+        
+
+        return null;
+    }
+}
